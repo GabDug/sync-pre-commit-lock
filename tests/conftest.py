1929472024ebf81eff4e20c233ef6e921fa12c65
Axel H. <noirbizarre@gmail.com>
2024-09-25 00:40:56 +0200

    test(pdm): adds some integration test for `pdm` plugin

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+import pytest
+
 try:
     import pdm  # noqa: F401
 except ImportError:
@@ -6,3 +10,8 @@ else:
     pytest_plugins = [
         "pdm.pytest",
     ]
+
+
+@pytest.fixture
+def fixtures() -> Path:
+    return Path(__file__).parent.joinpath("fixtures")
